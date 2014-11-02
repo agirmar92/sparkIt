c485997c84481e7f5f5908dd42caa52c745fc13f
Ægir Már Jónsson <aegir13@ru.is>
2014-11-02 17:07:48 +0000

    port fix

--- src/selenium/java/is/ru/sparkIt/ChuckWebTest.java
+++ src/selenium/java/is/ru/sparkIt/ChuckWebTest.java
@@ -26,7 +26,7 @@ public class ChuckWebTest {
         driver = new FirefoxDriver();
         port = System.getenv("PORT");
         if(port == null) {
-            port = "4567";
+            port = "4572";
         }
         baseUrl = "http://localhost:" + port;
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
